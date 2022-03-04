4154006c4ecc1fef6158a4c4b1e8db91fdc6ce4f
ArpitPundir <thakurarpitpundir73@gmail.com>
2022-03-04 23:13:01 +0530

    adding approach 2 for design min stack

--- Design-MIn-Stack.cpp
+++ Design-MIn-Stack.cpp
@@ -25,4 +25,33 @@ public:
     int getMin() {
         return data.back().second;
     }
-};`
+};
+
+class MinStack {
+    private: 
+    vector<pair<int,int> > data;
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if(!data.empty()){
+            int newMin = min(val, this->getMin());
+            data.push_back({val, newMin});
+        }else{
+            data.push_back({val, val});
+        }
+    }
+    
+    void pop() {
+        data.pop_back();
+    }
+    
+    int top() {
+        return data.back().first;
+    }
+    
+    int getMin() {
+        return data.back().second;
+    }
+};
